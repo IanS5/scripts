587864d512de5e0dc33b6886fdeeca935958e690
Ian Shehadeh <IanShehadeh2020@gmail.com>
2018-02-15 15:42:55 +0000

    made dirsync a bit more friendly

--- dirsync.sh
+++ dirsync.sh
@@ -1,23 +1,77 @@
 #!/bin/bash
-TARGET=$2
-SOURCE=$1
 
-mkdir -p "$TARGET" "$SOURCE"
-while [ 1 ]; do
-	IFS=';' read -r -a event_file <<< "`inotifywait -r --format '%e;%f' -e move -e create -e delete $1 $SOURCE`"
+CALL_NAME="$0"
+ARGS=("$@")
 
-	if [ "$?" != '0' ]; then exit 1; fi
+_usage() {
+    echo "USAGE: $CALL_NAME [-h] [-l] [-r] SOURCE TARGET\n"
+    printf "\tsync the contents of TARGET with SOURCE, the connection is one way.\n"
+	printf "OPTIONS:\n"
+    printf "\t-h: print this message and exit\n"
+    printf "\t-l: hard link files instead of copying them\n"
+    printf "\t-r: recursively sync all subdirectories of SOURCE\n"
 
-	event="${event_file[0]}"
-	file="${event_file[1]}"
-	printf "\nevent = \"%s\"\nfile = \"%s\"\n\n" "$event" "$file"
-	case "$event" in
-		CREATE | MOVE_TO)
-			mkdir -p "`dirname "$TARGET/$file"`"
-			cp -al "$SOURCE/$file" "$TARGET/$file"	
+}
+
+FLAG_STOP=`expr "$#" - 2`
+
+if [[ $FLAG_STOP -lt 0 ]]; then
+	_usage
+	exit 1
+fi
+
+LINKS=''
+RECURSIVE=''
+for i in `seq 0 $FLAG_STOP`; do
+	case "${ARGS[$i]}" in
+		'-h') 
+			_usage
+			exit 0
 			;;
-		DELETE | MOVE_FROM)
-			rm  -rf "$TARGET/$file"
-			;;
+		'-r') RECURSIVE='1';;
+		'-l') LINKS='1';;
 	esac
+done
+
+
+if [ -z "$RECURSIVE" ]; then
+	WAIT_COMMAND="inotifywait -q --format %,e;%f;%w -e move -e create -e delete"
+else
+	WAIT_COMMAND="inotifywait -rq --format %,e;%f;%w -e move -e create -e delete"
+fi
+
+if [ -z "$LINKS" ]; then
+	CP_COMMAND='cp -p'
+	WAIT_COMMAND="$WAIT_COMMAND -e close_write -e attrib -e modify"
+else
+	CP_COMMAND='ln '
+fi
+
+
+SOURCE=${ARGS[$FLAG_STOP]}
+TARGET=${ARGS[$(expr 1 + $FLAG_STOP)]}
+
+while true; do
+	wait_response=`$WAIT_COMMAND $SOURCE`
+	if [[ $? -ne 0 ]]; then exit $?; fi
+
+	IFS=';'  read -r -a event_file <<< "$wait_response"
+	IFS=', ' read -r -a events <<< "${event_file[0]}"
+	file="${event_file[1]}"
+	path="${event_file[2]}"
+
+	for event in $events; do
+		case "$event" in
+			"CREATE" | "MOVE_TO" | "MODIFY" | "CLOSE_WRITE" | "ATTRIB")
+				if [[ -d $path/$file ]]; then 
+					mkdir -p "${path//$SOURCE/$TARGET}/$file";
+				else
+					`$CP_COMMAND "$path/$file" "${path//$SOURCE/$TARGET}/$file"`
+				fi
+				;;
+			"DELETE" | "MOVE_FROM")
+				rm  -rf "$TARGET/$file"
+				;;
+		esac
+	done
 done
