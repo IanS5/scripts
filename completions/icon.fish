f1f07b0e32855c60dc2ab13a6aee76fbb6597ede
Ian Shehadeh <IanShehadeh2020@gmail.com>
2018-06-24 09:33:56 -0400

    update completions

--- completions/icon.fish
+++ completions/icon.fish
@@ -1,1 +1,5 @@
-complete -f -c icon -a "(icon -n -a (commandline -t))"
+complete -f -c icon -a "(icon -na (commandline -t))"
+complete -c icon -s a -l names
+complete -c icon -s e -l escaped
+complete -c icon -s s -l search
+complete -c icon -s n -l newlines
--- completions/proj.fish
+++ completions/proj.fish
@@ -1,28 +1,9 @@
 #!/usr/bin/fish
 
-set -l commands "project" "template" "cd"
-set -l project_actions "recover" "backup" "create" "find"
-
-complete -f -c proj -x -n "not __ians5_string_in_list (__ians5_get_arg 1) $commands" -a "$commands"
-complete -f -c proj -x -n "__ians5_proj_command 'cd'" -a "(proj project)"
-complete -f -c proj -x -n "__ians5_proj_command 'project';  and not __ians5_proj_action $project_actions" -a "$project_actions"
-complete -f -c proj -x -n "__ians5_proj_command 'project';  and __ians5_proj_action 'find' 'create'"
-complete -f -c proj -x -n "__ians5_proj_command 'project';  and __ians5_proj_action 'remove' 'backup'" -a "(proj project fiond)"
-complete -f -c proj -x -n "__ians5_proj_command 'project';  and __ians5_proj_action 'create'; and test -n (__ians5_get_arg 3)" -a "(proj template find)"
-complete -f -c proj -x -n "__ians5_proj_command 'template'; and __ians5_proj_action 'create'"
-complete -f -c proj -x -n "__ians5_proj_command 'template'; and __ians5_proj_action 'remove' 'backup'" -a "(proj template find)"
-
-function __ians5_get_arg
-    commandline | string split ' ' | head -n(expr $argv[1] + 1) | tail -n1
-end
-
-function __ians5_string_in_list
-    if string match -- $argv[1] $argv[2..-1] > /dev/null
-        return 0
-    else
-        return 1
-    end
-end
-
-alias __ians5_proj_command='__ians5_string_in_list (__ians5_get_arg 1)'
-alias __ians5_proj_action='__ians5_string_in_list (__ians5_get_arg 2)'
+complete -c proj -s l -l list
+complete -c proj -s c -l create
+complete -c proj -s r -l remove
+complete -c proj -s b -l backup
+complete -c proj -s e -l recover
+complete -c proj -s P -l project -a 'proj -lP""'
+complete -c proj -s T -l template -a 'proj -lT""'
