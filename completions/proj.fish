8f05d841d702a23cf0ab246800cb4b0813c3fc99
Ian Shehadeh <IanShehadeh2020@gmail.com>
2018-06-24 10:28:14 -0400

    fix proj completions

--- completions/proj.fish
+++ completions/proj.fish
@@ -1,9 +1,13 @@
 #!/usr/bin/fish
 
-complete -c proj -s l -l list
-complete -c proj -s c -l create
-complete -c proj -s r -l remove
-complete -c proj -s b -l backup
-complete -c proj -s e -l recover
-complete -c proj -s P -l project -a 'proj -lP""'
-complete -c proj -s T -l template -a 'proj -lT""'
+complete -c proj -x
+complete -c proj -x -n 'test -z (commandline -o | sed -n 2p)' -a "cd"
+complete -c proj -x -n 'string match -- (commandline -o | sed -n 2p) cd' -a "(proj -lP'.*')"
+
+complete -c proj -x -s l -l list
+complete -c proj -x -s c -l create
+complete -c proj -x -s r -l remove
+complete -c proj -x -s b -l backup
+complete -c proj -x -s e -l recover
+complete -c proj -x -s P -l project -a "(proj -lP'.*')"
+complete -c proj -x -s T -l template -a "(proj -lT'.*')"
